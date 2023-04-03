0289d3c0a1bc5579b1f402cde6a4a109408abd23
tolujohn <tolujohnofficial@gmail.com>
2023-04-03 17:27:51 +0100

    header file added

---
+++ src/Heading.js
@@ -0,0 +1,7 @@
+function Header() {
+    return (
+        <h1>This is the Header of this Project</h1>
+    )
+}
+
+export default Header;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+function Header() {
+    return (
+        <h4>This is the Header of this Project</h4>
+    )
+}
+
+export default Header;
